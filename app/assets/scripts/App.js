a143393468ce41ebbaf4555556758a0297b378bd
Julien Orjollet <oj.professionnel@gmail.com>
2021-03-11 23:22:54 +0100

    step back to original settings, removed all barbajs pre-code html, keeping only the new HTML organization with wrapper/progress bar and body, page-transitions.css on the side for now

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,56 @@
 import '../styles/styles.css';
 
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
 import mobileMenu from './modules/mobileMenu';
 
+const body = document.querySelector('body');
+
 
 mobileMenu();
+
+barba.use(barbaCss);
+
+
+
+/**  GLOBAL HOOK TO HANDLE THE BODY BCG COLOR  **/
+barba.hooks.before((data) => {
+
+  const background = data.current.container.dataset.background;
+  /**  SET NEW VALUES FORM THE DATA-BCG  TO THE CSS CLASS **/
+  body.style.setProperty('--page-background', background);
+});
+
+
+barba.init({
+  /*  IN AN ARRAY  */
+  transitions: [
+    {
+      name: 'home',
+      to: {
+        namespace: ['home']
+      },
+      once: () => { },
+      leave: () => { },
+      enter: () => { },
+    }, {
+      name: 'slide',
+      leave: () => { },
+      enter: () => { },
+      from: {
+        namespace: ['home2']
+      },
+      to: {
+        namespace: ['home']
+      }
+    }
+  ]
+
+});
+
+
+
 
 
 if (module.hot) {
