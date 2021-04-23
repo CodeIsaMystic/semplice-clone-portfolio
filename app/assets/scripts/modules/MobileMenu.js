c9aaf402cd00d9aa48fcfdaced192b773c0c3dbc
Julien Orjollet <oj.professionnel@gmail.com>
2021-04-23 12:58:41 +0200

    border &fixed position on navbar--is-visible

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@
 function mobileMenu() {
   const menuBtn = document.querySelector(".menu-btn");
   const navBar = document.querySelector(".navbar");
+  const topBar = document.querySelector(".top-bar");
   const siteHeader = document.querySelector(".site-header");
 
 
@@ -11,6 +12,7 @@ function mobileMenu() {
 
     /// ************************************
     navBar.classList.toggle("navbar--is-visible");
+    topBar.classList.toggle("top-bar--is-visible");
     siteHeader.classList.toggle("site-header--is-expanded");
     menuBtn.classList.toggle("menu-btn--closeX");
   }
